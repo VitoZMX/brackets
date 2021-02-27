16986ffa4d5de87b138d55c6c00c1d35ae42253c
VitoZMX <Vi-69@yandex.ru>
2021-02-27 13:14:34 +0300

    feat: add brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    const nAr = bracketsConfig.map((el) => el.join(''));
+
+
+    for (let i = 0; i < nAr.length;) {
+        if (str.includes(nAr[i])) {
+            str = str.replace(nAr[i], '');
+            i = 0;
+            continue;
+        }
+        i++;
+    }
+    if (str.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
+
+};
